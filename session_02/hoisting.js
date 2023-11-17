7651cfc0548c359f1dabeab2ebd80c1677030164
jin-jae <jinjae.dev@gmail.com>
2023-11-17 19:20:01 +0900

    add: Front session #2 materials

---
+++ session_02/hoisting.js
@@ -0,0 +1,3 @@
+// hoisting is a process of moving all the declarations to the top of the file
+console.log(a)
+var a = "Hello"
---
+++ session_02/var_let.js
@@ -0,0 +1,12 @@
+let a = 2
+// let a = 3
+a = 3
+a = 512
+a = "hello"
+
+var b = 2
+var b = 5
+
+
+const c = 1
+// c = 2
