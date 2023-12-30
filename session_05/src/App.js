43ca3958850b27f4e7f0da72d8577bdc7a92859a
jin-jae <jinjae.dev@gmail.com>
2023-12-30 20:17:09 +0900

    add: Front session #5 materials

---
+++ session_05/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GDSC Kookmin University</h1>
+      <h2>Frontend Session</h2>
+    </div>
+  );
+}
+
+export default App;
