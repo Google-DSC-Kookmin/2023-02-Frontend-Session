7a6f45b0c9117528ad4a0a22692333515b93fb7c
CSH2315 <choish00123@gmail.com>
2024-02-06 03:24:23 +0900

    최승훈-과제01-1

--- assignments_01/PGM_hate_same_number.js
+++ assignments_01/PGM_hate_same_number.js
@@ -1,9 +1,16 @@
-function solution(arr)
-{
-  var answer = [];
+function solution(arr) {
+  var answer = [arr[0]];
+  let current = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== current) {
+      answer.push(arr[i]);
+      current = arr[i];
+    }
+  }
 
   // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-  console.log('Hello Javascript')
+  console.log(answer);
 
   return answer;
 }
--- assignments_01/PGM_largest_number.js
+++ assignments_01/PGM_largest_number.js
@@ -1,4 +1,12 @@
 function solution(numbers) {
-  var answer = '';
+  let sortedNum = numbers.map((num) => num.toString());
+
+  sortedNum.sort((a, b) => (b + a).localeCompare(a + b));
+
+  if (sortedNum[0] === "0") {
+    return "0";
+  }
+
+  var answer = sortedNum.join("");
   return answer;
 }
